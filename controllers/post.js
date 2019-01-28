db1199972126e375afe6fd65ae21f7e8ff9c40ca
Thomas <thomaslee9474@gmail.com>
2019-01-27 21:28:07 -0800

    [added] users can now see individual posts

--- controllers/post.js
+++ controllers/post.js
@@ -1,6 +1,7 @@
 const Post = require('../models/post');
 
 module.exports = (app) => {
+  // SHOW POSTS
   app.get('/', (req, res) => {
     Post.find({})
       .then((posts) => {
@@ -11,15 +12,30 @@ module.exports = (app) => {
       });
   });
 
+  // RENDER NEW-POST PAGE
   app.get('/posts/new', (req, res) => {
     res.render('new-post')
   })
-  // CREATE
+
+  // CREATE NEW POST
   app.post('/posts/new', (req,res) => {
     // INSTANTIATE INSTANCE OF POST MODEL
     const post = new Post(req.body);
-
     // SAVE INSTANCE OF POST MODEL TO DB
     post.save((err, post) => res.redirect('/'));
+  });
+
+  // SEE INDIVIDUAL POST 
+  app.get('/post/:id', (req, res) => {
+    console.log("************************")
+    console.log(req.params.id)
+    // LOOK UP POST
+    Post.findById(req.params.id)
+      .then((post) => {
+        res.render('posts-show', {post});
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
   });
 };
