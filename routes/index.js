c2072aedc9cc06664f95d667188a4ba4d4580d5a
Thomas <thomaslee9474@gmail.com>
2019-02-04 16:18:20 -0800

    [changes] for dani

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,10 @@ const Post = require('../models/post');
 
 // SHOW POSTS
 router.get('/', (req, res) => {
+  const currentUser = req.user; 
+  console.log(currentUser)
   Post.find({})
     .then((posts) => {
-      const currentUser = req.user; 
       res.render('post-index', { posts, currentUser }); 
     })
     .catch((err) => {console.log(err.message); });
