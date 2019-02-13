6ac30a6e5aa4af8af7427d8bfeb653d8166655dd
Thomas <thomaslee9474@gmail.com>
2019-02-12 16:31:02 -0800

    [added] users can up/downvote

--- routes/post.js
+++ routes/post.js
@@ -2,15 +2,14 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 const express = require('express');
-
 const router = express.Router();
 
-// AUTH CUSTOM MIDDLEAWARE
+// CUSTOM MIDDLEWARE
 const checkAuth = require('../middleware/checkAuth');
 
 // RENDER NEW-POST PAGE
 router.get('/new', checkAuth, (req, res) => {
-  const currentUser = req.user; 
+  const currentUser = req.user;
   res.render('new-post', { currentUser });
 }); 
 
@@ -23,15 +22,22 @@ router.post('/new', checkAuth, (req,res) => {
     // INSTANTIATE INSTANCE OF POST MODEL
     const post = new Post(req.body);
     post.author = req.user._id
+    post.upVotes = [];
+    post.downVotes = [];
+    post.voteScore = 0;
+    let id
     // SAVE INSTANCE OF POST MODEL TO DB
     post
       .save()
-      .then((post) => { return User.findById(req.user._id) })
+      .then((post) => { 
+        id = post._id
+        return User.findById(req.user._id) 
+      })
       .then((user) => {
         user.posts.unshift(post._id);
         user.save();
         // REDIRECT TO NEW POST
-        res.redirect(`/posts/${post._id}`)
+        res.redirect(`/posts/${id}`)
       })
       .catch((err) => {
         console.log(err.message); 
@@ -42,14 +48,33 @@ router.post('/new', checkAuth, (req,res) => {
   }
 });
 
+router.put('/:id/vote-up', checkAuth, (req, res) => {
+  Post.findById(req.params.id).exec((err, post) => {
+    post.upVotes.push(req.user._id);
+    post.voteScore += 1;
+    post.save();
+
+    res.status(200);
+  });
+});
+
+router.put('/:id/vote-down', checkAuth, (req, res) => {
+  Post.findById(req.params.id).exec((err, post) => {
+    post.downVotes.push(req.user._id);
+    post.voteScore -= 1;
+    post.save();
+
+    res.status(200);
+  });
+});
+
 // SEE INDIVIDUAL POST AND NESTED COMMENTS
 router.get('/:id', checkAuth, (req, res) => {
   // LOOK UP POST
   const currentUser = req.user; 
-  console.log(currentUser);
+  console.log("hello");
   Post.findById(req.params.id).populate('comments').lean()
     .then((post) => {
-      console.log(post.comments[0].comments);
       res.render('posts-show', { post, currentUser });
     })
     .catch((err) => { console.log(err.message); });
